c6e7f18f9a9d5bb7b5956910cab30ffb10e96827
gustavoburgie <gustavoburgie@gmail.com>
2025-08-21 05:54:47 -0300

    opa

---
+++ README.md
@@ -0,0 +1,4 @@
+vamos ver se funciona essa paradinha!
+1. afsgdf
+1. dfgdgbhgfhbgf
+1.  gredgdgbfbfcgb
--- main.c
+++ main.c
@@ -5,8 +5,23 @@
 int main(){
     printf("\nabroba\n\n");
     char *str = malloc(sizeof(char) * 100);
-    scanf("%s", &str);
+    int scan = scanf("%s", str);
+    if(scan == 0){
+        printf("Erro ao ler a entrada.\n");
+        free(str);
+        return 1;
+    }
+    
     printf("Voce digitou: %s\n", str);
+    free(str);
+
+    printf("Menu:\n");
+    printf("1 - Start game\n");
+    printf("2 - Regras:\n");
+    printf("3 - Sair\n");
+
+    str = malloc(sizeof(char) * 2);
+
 
     free(str);
     return 0;
